4d0d78009d6299f855cc4a36a98f959209f5127a
tektechnologies <info@tektechnologies.com>
2019-07-25 09:38:57 -0500

    Add Class 14 Lab Read me

---
+++ class-14/lab/starter-code/js/app.js
@@ -0,0 +1,42 @@
+/* global Product, Cart */
+
+'use strict';
+
+// TODO: Create a "Cart" constructor (named Cart) that has item and quantity
+// properties, and builds an array of items as you create instances
+
+
+
+// Product Contructor
+var Product = function(filePath, name) {
+  this.filePath = filePath;
+  this.name = name;
+  Product.allProducts.push(this);
+};
+Product.allProducts = [];
+
+function generateCatalog() {
+  new Product('assets/bag.jpg', 'Bag');
+  new Product('assets/banana.jpg', 'Banana');
+  new Product('assets/bathroom.jpg', 'Bathroom');
+  new Product('assets/boots.jpg', 'Boots');
+  new Product('assets/breakfast.jpg', 'Breakfast');
+  new Product('assets/bubblegum.jpg', 'Bubblegum');
+  new Product('assets/chair.jpg', 'Chair');
+  new Product('assets/cthulhu.jpg', 'Cthulhu');
+  new Product('assets/dog-duck.jpg', 'Dog-Duck');
+  new Product('assets/dragon.jpg', 'Dragon');
+  new Product('assets/pen.jpg', 'Pen');
+  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
+  new Product('assets/scissors.jpg', 'Scissors');
+  new Product('assets/shark.jpg', 'Shark');
+  new Product('assets/sweep.png', 'Sweep');
+  new Product('assets/tauntaun.jpg', 'Taun-Taun');
+  new Product('assets/unicorn.jpg', 'Unicorn');
+  new Product('assets/usb.gif', 'USB');
+  new Product('assets/water-can.jpg', 'Water Can');
+  new Product('assets/wine-glass.jpg', 'Wine Glass');
+}
+
+// Initialize the app by creating the big list of products with images and names
+generateCatalog();
---
+++ class-14/lab/starter-code/js/cart.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Cart = [];
+
+// Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
+var table = document.getElementById('cart');
+table.addEventListener('click', removeItemFromCart);
+
+function loadCart() {
+  Cart = JSON.parse(localStorage.getItem('cart')) || [];
+}
+
+// Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
+function renderCart() {
+  loadCart();
+  clearCart();
+  showCart();
+}
+
+// TODO: Remove all of the rows (tr) in the cart table (tbody)
+function clearCart() {}
+
+// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
+function showCart() {
+
+  // TODO: Find the table body
+
+  // TODO: Iterate over the items in the cart
+  // TODO: Create a TR
+  // TODO: Create a TD for the delete link, quantity,  and the item
+  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+}
+
+function removeItemFromCart(event) {
+
+  // TODO: When a delete link is clicked, rebuild the Cart array without that item
+  // TODO: Save the cart back to local storage
+  // TODO: Re-draw the cart table
+
+}
+
+// This will initialize the page and draw the cart on screen
+renderCart();
